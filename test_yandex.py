f8fdd629878964d8ffd688d1d9ca39491f11cb52
penguin_PC <djerrylinux@gmail.com>
2021-11-13 15:25:22 +0000

    Были исправлены ошибки с вызовом элементов из словаря.

--- test_yandex.py
+++ test_yandex.py
@@ -1,8 +1,3 @@
-from unittest import TestCase
-from .local_config import *
-from .fosy_proxy_tools import *
-from .main import register_yandex_account
-
 from random import choice
 from unittest import TestCase
 from local_config import *
@@ -13,21 +8,24 @@ from main import *
 class Test(TestCase):
     def test_callback_yandex(self):
         fosy_proxy = FosyProxyTools(FOSY_KEY)
-        current_proxy = fosy_proxy.get_proxy("http")
-        register_yandex_account(current_proxy, TEST_RECEIVER, TWOCAPTCHA_TOKEN)
-        self.fail("Not finished")
-        
         receivers = [TEST_RECEIVER]
         accounts = []
-        for i in range(0,10):
+        for i in range(0, 5):
             current_proxy = fosy_proxy.get_proxy("http")
             account = register_yandex_account(current_proxy, choice(receivers), TWOCAPTCHA_TOKEN)
-            if (account.result == "Успех!"):
-                receivers.append(account.login)
+            print(account['result'])
+            print(account['mail'])
+            print(account['password'])
+            if account['result'] == "Успех!":
+                receivers.append(account['mail'])
                 accounts.append(account)
+        if len(accounts) > 0:
+            for account in accounts:
+                current_proxy = fosy_proxy.get_proxy("http")
+                result = fake_auth_yandex(current_proxy, account['mail'], account['password'])
+                self.assertRegex(result, "^Успех:.*", "Result: "+result)
 
-        for account in accounts:
-            current_proxy = fosy_proxy.get_proxy("http")
-            result = fake_auth_yandex(current_proxy, account.login, account.password)
-            self.assertRegex(result, "^Успех:.*", "Result: "+result)
 
+if __name__ == '__main__':
+    t = Test()
+    t.test_callback_yandex()
