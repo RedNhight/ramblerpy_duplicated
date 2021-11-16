c6bb453aee200c4c86b49acf1a7bc4c364d53abc
penguin_PC <djerrylinux@gmail.com>
2021-11-16 03:29:45 +0000

    Был исправлен фильтр текста.

--- utils/pars_answers.py
+++ utils/pars_answers.py
@@ -16,11 +16,13 @@ class ParsRandomPoety:
         for elem in pre:
             if elem == '\n':
                 pre.remove(elem)
-        if ' ' not in pre[2].strip(',').strip('')[0]:
-            return [pre[2].strip(',').strip('').replace('\n', '')[0:10], pre[2].strip(',').strip('').replace('\n', '') + ' ' + pre[3].strip(',').strip('').replace('\n', '')]
-        else:
-            return [pre[3].strip(',').strip('').replace('\n', '')[0:10], pre[3].strip(',').strip('').replace('\n', '') + ' ' + pre[4].strip(',').strip('').replace('\n', '')]
-
+        pre = list(filter(None, pre))
+        return [pre[0].lstrip().replace('\n', '')[0:15], pre[0].lstrip().replace('\n', '') + '\n' + pre[1].lstrip().replace('\n', '')]
+        # if ' ' not in pre[2].strip(',').strip('')[0]:
+        #     return [pre[2].strip(',').strip('').replace('\n', '')[0:10], pre[2].strip(',').strip('').replace('\n', '') + ' ' + pre[3].strip(',').strip('').replace('\n', '')]
+        # else:
+        #     return [pre[3].strip(',').strip('').replace('\n', '')[0:10], pre[3].strip(',').strip('').replace('\n', '') + ' ' + pre[4].strip(',').strip('').replace('\n', '')]
+        #
 
 if __name__ == '__main__':
     prp = ParsRandomPoety()
